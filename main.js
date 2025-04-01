2ce5453193b5be4f957b9bf9ee8893f3b2fa5a44
luciameneghin <lucia.meneghin00@gmail.com>
2025-04-01 19:20:58 +0200

    feat: snack 1, without bonus

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('ciao')
+// function getPostTitle(id) {
+//   return fetch(`https://dummyjson.com/posts/${id}`)
+//     .then(response => response.json())
+//     .then(post => console.log(`Titolo del post: ${post.title}`))
+//     .catch(error => console.error("Errore:", error));
+// }
+
+// getPostTitle(6)
+
+const getPostTitle = id => {
+  const promessa = new Promise((resolve, reject) => {
+    fetch(`https://dummyjson.com/posts/${id}`)
+      .then(response => response.json())
+      .then(post => resolve(post.title))
+      .catch(reject);
+  });
+  return promessa
+}
+
+getPostTitle(2)
+  .then(post => console.log(`Titolo del post: ${post}`))
+  .catch(error => console.error("Errore:", error));
