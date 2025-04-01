accf04e00ffbdd44dc3ff76d34513fdbd69faa3c
luciameneghin <lucia.meneghin00@gmail.com>
2025-04-01 19:33:24 +0200

    feat: snack 2, without bonus

--- main.js
+++ main.js
@@ -20,3 +20,23 @@ const getPostTitle = id => {
 getPostTitle(2)
   .then(post => console.log(`Titolo del post: ${post}`))
   .catch(error => console.error("Errore:", error));
+
+
+
+
+function lanciaDado() {
+  const promessaDado = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() <= 0.2) {
+        reject('Il dado si è incastrato, ricarica la pagina')
+      } else {
+        resolve(Math.floor(Math.random() * 6) + 1)
+      }
+    }, 3000)
+  })
+  return promessaDado
+}
+
+lanciaDado()
+  .then(result => console.log("Risultato del dado:", result))
+  .catch(error => console.error("Errore:", error))
