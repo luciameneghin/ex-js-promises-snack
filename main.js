65fcff01984851415ddfac0f156b25fd87f285c3
luciameneghin <lucia.meneghin00@gmail.com>
2025-04-02 16:13:23 +0200

    feat: added bonus snack 2

--- main.js
+++ main.js
@@ -7,6 +7,7 @@
 
 // getPostTitle(6)
 
+//SNACK 1
 const getPostTitle = id => {
   const promessa = new Promise((resolve, reject) => {
     fetch(`https://dummyjson.com/posts/${id}`)
@@ -48,9 +49,10 @@ getPost(5)
 
 
 
-
+//SNACK 2
 function lanciaDado() {
   const promessaDado = new Promise((resolve, reject) => {
+    console.log("Lancio il dado...")
     setTimeout(() => {
       if (Math.random() <= 0.2) {
         reject('Il dado si è incastrato, ricarica la pagina')
@@ -67,3 +69,37 @@ lanciaDado()
   .catch(error => console.error("Errore:", error))
 
 //BONUS SNACK 2
+const creaLanciaDado = () => {
+
+  let ultimoLancio = null;
+
+  return function () {
+    return new Promise((resolve, reject) => {
+      console.log("Lancio il dado...")
+      setTimeout(() => {
+        if (Math.random() <= 0.2) {
+          ultimoLancio = null;
+          reject('Il dado si è incastrato, ricarica la pagina')
+        } else {
+          const risultato = Math.floor(Math.random() * 6) + 1
+          if (risultato === ultimoLancio) {
+            console.log('Incredibile! Hai fatto lo stesso risultato del lancio precedente!')
+          }
+          ultimoLancio = risultato;
+          resolve(risultato)
+        }
+      }, 3000)
+    })
+  }
+}
+
+const lanciaDadoMemoria = creaLanciaDado();
+
+lanciaDadoMemoria()
+  .then(result => {
+    console.log("Risultato del dado con memoria (1):", result)
+    lanciaDadoMemoria()
+      .then(result => console.log("Risultato del dado con memoria (2):", result))
+      .catch(error => console.error("Errore:", error))
+  })
+  .catch(error => console.error("Errore:", error))
